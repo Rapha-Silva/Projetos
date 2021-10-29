345ff741400ed5b76c185fe1128c289999a43b20
Raphael Silva <92612115+Rapha-Silva@users.noreply.github.com>
2021-10-29 17:00:56 -0300

    Triângulo
    Prog. verifica a possibilidade de formar um triângulo com retas distintas.

--- teste_de_triangulo.py
+++ teste_de_triangulo.py
@@ -9,3 +9,4 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r2 + r1:
     print('É possível formar um triângulo com {} {} {}'.format(r1, r2, r3 ))
 else:
     print('Não é possível formar um triângulo')
+    
